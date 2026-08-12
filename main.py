b8aa6958e8fe3b29e82f40c16694a0b11946c9d6
Abdroz-w <vadimkandyba4@gmail.com>
2026-08-12 20:28:30 +0300

    тестовое приложение

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-print("Hello world")
-x = 123
-print("hdjfg")
-y = 55
+import tkinter as tk
+
+clicks = 0
+def count_clicks():
+    global clicks
+    clicks += 1
+    label_count.config(text=f"Нажатий:{clicks}")
+    if clicks > 10:
+        root.config(bg="#d4edda")
+        label_count.config(bg="#d4edda", fg="#155724")
+root = tk.Tk()
+root.title('Кликер')
+root.geometry("350x250")
+
+label_count = tk.Label(root, text="Нажатий: 0", font=("Arial", 16))
+label_count.pack(pady=30)
+
+btn_click = tk.Button(root, text="Кликни меня!", font=("Arial", 14), command=count_clicks, bg="#007bff", fg="white")
+btn_click.pack(pady=20)
+
+
+root.mainloop()
